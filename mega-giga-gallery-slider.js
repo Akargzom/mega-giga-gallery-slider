4376d021ca4682537c700b97d868c1da575445d9
Alexander Ilyashenko <83316201+Akargzom@users.noreply.github.com>
2022-04-12 02:22:49 +0300

    Initial commit

---
+++ mega-giga-gallery-slider.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function ($) {
+  if($('.MGGS_gallery-slider').hasClass('auto')){
+    atp  = true;
+    atps = $('.MGGS_gallery-slider').attr('data');
+  }else{
+    atp  = false;
+  }
+    $('.MGGS_gallery-slider').each(function() {
+        $(this).slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: atp,
+          autoplaySpeed: atps,
+          prevArrow: $(this).next('.MGGS_panel-control').find('.prev'),
+          nextArrow: $(this).next('.MGGS_panel-control').find('.next'),
+          dots: true,
+          appendDots: $(this).next('.MGGS_panel-control').find('.slider-dots'),
+          responsive: [{
+            breakpoint: 651,
+            settings: {
+              dots: false,
+            }
+          }]
+      });
+  });
+});
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Mega-giga-gallery-slider ===
+Contributors: Sashko Ilyashenko
+Tags: tabs
+Requires at least: 4.0
+Tested up to: 5.9.2
+Stable tag: 1.0
+Requires PHP: 5.6
+License: GPLv3
+License URI: 
+
+Makin ur gallery giga-mega!!!
+
+== Description ==
+Selectively makes your galleries to sliders, with options to customize it as u wish. 
+
+== Installation ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+
+== Upgrade Notice ==
+
