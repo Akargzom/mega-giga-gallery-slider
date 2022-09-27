03f7fe6a76b226496e2205f2a125231ec5498ff4
Alexander Ilyashenko <83316201+Akargzom@users.noreply.github.com>
2022-09-28 01:18:22 +0300

    Update mega-giga-gallery-slider.php

--- mega-giga-gallery-slider.php
+++ mega-giga-gallery-slider.php
@@ -17,7 +17,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('jquery');
     wp_enqueue_style('gs_style', plugins_url('mega-giga-gallery-slider.css', __FILE__));
     wp_enqueue_script('gs_js', plugins_url('mega-giga-gallery-slider.js', __FILE__));
-    if (get_option('slide_data')['MGGS_check_slick'] !== 1) {
+    if (isset(get_option('slide_data')['MGGS_check_slick']) && get_option('slide_data')['MGGS_check_slick'] != 1) {
         wp_enqueue_script('slick_js', plugins_url('slick.min.js', __FILE__));
         wp_enqueue_style('slick_css', plugins_url('slick.css', __FILE__));
     }
@@ -95,33 +95,34 @@ function MGGS_get_fields_example($args)
         echo '<input name="' . esc_attr($name_option) . '[' . esc_attr($field_name) . ']" type="number" id="' . esc_attr($field_name) . '" value="' . esc_attr($value) . '" class="regular-text" />';
     }
 }
-if (get_option('slide_data')['MGGS_check_gutenberg'] == 1) {
+$opts = get_option('slide_data');
+if (isset($opts['MGGS_check_gutenberg']) && $opts['MGGS_check_gutenberg'] == 1) {
     add_filter('render_block', function ($block_content, $block) {
-        if (get_option('slide_data')['MGGS_check_thumbnails'] == 1)
+        if (isset(get_option('slide_data')['MGGS_check_thumbnails']) && get_option('slide_data')['MGGS_check_thumbnails'] == 1)
             $thumb = ' thumb';
         else
             $thumb = '';
-        if (get_option('slide_data')['MGGS_nmb_thumbnails'])
+        if (isset(get_option('slide_data')['MGGS_nmb_thumbnails']) && get_option('slide_data')['MGGS_nmb_thumbnails'])
             $thumbs = get_option('slide_data')['MGGS_nmb_thumbnails'];
         else
             $thumbs = 5;
-            if (get_option('slide_data')['MGGS_nmb_t_thumbnails'])
+            if (isset(get_option('slide_data')['MGGS_nmb_t_thumbnails']) && get_option('slide_data')['MGGS_nmb_t_thumbnails'])
             $t_thumbs = get_option('slide_data')['MGGS_nmb_t_thumbnails'];
         else
             $t_thumbs = 4;
-            if (get_option('slide_data')['MGGS_nmb_m_thumbnails'])
+            if (isset(get_option('slide_data')['MGGS_nmb_n_thumbnails']) && get_option('slide_data')['MGGS_nmb_m_thumbnails'])
             $m_thumbs = get_option('slide_data')['MGGS_nmb_m_thumbnails'];
         else
             $m_thumbs = 3;
         $mar = '';
         if ($thumb == ' thumb')
             $mar = ' t-mar';
-            if (get_option('slide_data')['MGGS_select_columns']) {
+            if (isset(get_option('slide_data')['MGGS_select_columns']) && get_option('slide_data')['MGGS_select_columns']) {
                 $clmn = get_option('slide_data')['MGGS_select_columns'];
             } else {
                 $clmn = 1;
             }
-            if (get_option('slide_data')['MGGS_nmb_autospeed']) {
+            if (isset(get_option('slide_data')['MGGS_nmb_autospeed']) && get_option('slide_data')['MGGS_nmb_autospeed']) {
                 $atps = get_option('slide_data')['MGGS_nmb_autospeed'];
             } else {
                 $atps = 3000;
@@ -130,7 +131,7 @@ if (get_option('slide_data')['MGGS_check_gutenberg'] == 1) {
                 $ids = $block['attrs']['ids'] ?? array_column(array_column($block['innerBlocks'], 'attrs'), 'id');
                 $size = $block['attrs']['sizeSlug'];
                 $clmn_clss = '';
-                if (get_option('slide_data')['MGGS_check_autoscroll'] == 1) {
+                if (isset(get_option('slide_data')['MGGS_check_autoscroll']) && get_option('slide_data')['MGGS_check_autoscroll'] == 1) {
                     $clmn_clss = ' auto';
                 }
                 $output = '';
@@ -289,36 +290,36 @@ if (get_option('slide_data')['MGGS_check_gutenberg'] == 1) {
         $selector = "gallery-{$instance}";
 
         $gallery_style = '';
-        if (get_option('slide_data')['MGGS_select_columns']) {
+        if (isset(get_option('slide_data')['MGGS_select_columns']) && get_option('slide_data')['MGGS_select_columns']) {
             $clmn = get_option('slide_data')['MGGS_select_columns'];
         } else {
             $clmn = 1;
         }
-        if (get_option('slide_data')['MGGS_nmb_autospeed'])
+        if (isset(get_option('slide_data')['MGGS_nmb_autospeed']) && get_option('slide_data')['MGGS_nmb_autospeed'])
             $atps = get_option('slide_data')['MGGS_nmb_autospeed'];
         else
             $atps = 3000;
         if ($atts['columns'] == $clmn) {
             $clmn_clss = '';
-            if (get_option('slide_data')['MGGS_check_autoscroll'] == 1) {
+            if (isset(get_option('slide_data')['MGGS_check_autoscroll']) && get_option('slide_data')['MGGS_check_autoscroll'] == 1) {
                 $clmn_clss = ' auto';
             }
-            if (get_option('slide_data')['MGGS_check_thumbnails'] == 1)
+            if (isset(get_option('slide_data')['MGGS_check_thumbnails']) && get_option('slide_data')['MGGS_check_thumbnails'] == 1)
                 $thumb = ' thumb';
             else
                 $thumb = '';
-            if (get_option('slide_data')['MGGS_nmb_thumbnails'])
+            if (isset(get_option('slide_data')['MGGS_nmb_thumbnails']) && get_option('slide_data')['MGGS_nmb_thumbnails'])
                 $thumbs = get_option('slide_data')['MGGS_nmb_thumbnails'];
             else
                 $thumbs = 5;
             $mar = '';
             if ($thumb == ' thumb')
                 $mar = ' t-mar';
-                if (get_option('slide_data')['MGGS_nmb_t_thumbnails'])
+                if (isset(get_option('slide_data')['MGGS_nmb_t_thumbnails']) && get_option('slide_data')['MGGS_nmb_t_thumbnails'])
                 $t_thumbs = get_option('slide_data')['MGGS_nmb_t_thumbnails'];
             else
                 $t_thumbs = 4;
-                if (get_option('slide_data')['MGGS_nmb_m_thumbnails'])
+                if (isset(get_option('slide_data')['MGGS_nmb_n_thumbnails']) && get_option('slide_data')['MGGS_nmb_m_thumbnails'])
                 $m_thumbs = get_option('slide_data')['MGGS_nmb_m_thumbnails'];
             else
                 $m_thumbs = 3;
